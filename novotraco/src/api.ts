cb1d438f04afaa8af26000e38b23bc4b78421262
NikollasBeltrao <64939921+NikollasBeltrao@users.noreply.github.com>
2023-06-06 00:15:58 -0300

    a

--- novotraco/src/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-    //baseURL: 'https://backend-analise.000webhostapp.com/novotraco/',     
-    baseURL: 'http://localhost/novotraco/api/',     
-});
-
-export default api;
